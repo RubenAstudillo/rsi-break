5c452348db1a437d89672c29d1746419b7a9105a
Ruben Astudillo <ruben.astud@gmail.com>
2023-09-22 18:45:53 -0300

    Correct dings on popwin

--- popup/Main.hs
+++ popup/Main.hs
@@ -3,16 +3,16 @@
 
 module Main where
 
+import Control.Concurrent (forkIO)
+import Data.Functor (void)
+import Data.Maybe (fromMaybe, listToMaybe)
 import Data.String (IsString (..))
 import Monomer
 import Paths_rsi_break
-import System.Timeout (timeout)
-import Data.Functor (void)
 import System.Environment (getArgs)
-import Data.Maybe (fromMaybe, listToMaybe)
-import System.Process (runInteractiveProcess, waitForProcess)
-import System.Exit (ExitCode)
-import Control.Concurrent (forkIO)
+import System.IO (IOMode (..), openFile)
+import System.Process
+import System.Timeout (timeout)
 
 main :: IO ()
 main = do
@@ -21,7 +21,7 @@ main = do
     windowIconPath <- fromString <$> getDataFileName "assets/images/icon.png"
     robotoRegularFont <- fromString <$> getDataFileName "assets/fonts/Roboto-Regular.ttf"
     let cfg = config windowIconPath robotoRegularFont
-    _ <- forkIO $ void dingBell
+    _ <- forkIO dingBell
     void . timeout 5_000_000 $ startApp () (\_ _ _ _ -> []) (buildUI str) cfg
   where
     config icon' roboto =
@@ -37,8 +37,16 @@ buildUI str _wenv _model =
         . animFadeIn_ [autoStart, duration 4_000]
         $ label (fromString str) `styleBasic` [textSize 80.0]
 
-dingBell :: IO ExitCode
+dingBell :: IO ()
 dingBell = do
+    outHandle <- openFile "/dev/null" WriteMode
+    inHandle <- openFile "/dev/null" ReadMode
     soundPath <- getDataFileName "assets/sound/bell.mp3"
-    (_, _, _, phandle) <- runInteractiveProcess "mpv" ["--no-video", soundPath] Nothing Nothing
-    waitForProcess phandle
+    let process =
+            (shell (unwords ["mpv", "--no-video", soundPath]))
+                { std_in = UseHandle inHandle
+                , std_out = UseHandle outHandle
+                , std_err = UseHandle outHandle
+                }
+    (_, _, _, phandle) <- createProcess process
+    void (waitForProcess phandle)
