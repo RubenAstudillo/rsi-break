e45bd884a13c339c1fc1526e3d5c058e28133a3c
Ruben Astudillo <ruben.astud@gmail.com>
2023-09-22 16:46:57 -0300

    popwin plays sound

--- popup/Main.hs
+++ popup/Main.hs
@@ -10,6 +10,9 @@ import System.Timeout (timeout)
 import Data.Functor (void)
 import System.Environment (getArgs)
 import Data.Maybe (fromMaybe, listToMaybe)
+import System.Process (runInteractiveProcess, waitForProcess)
+import System.Exit (ExitCode)
+import Control.Concurrent (forkIO)
 
 main :: IO ()
 main = do
@@ -18,6 +21,7 @@ main = do
     windowIconPath <- fromString <$> getDataFileName "assets/images/icon.png"
     robotoRegularFont <- fromString <$> getDataFileName "assets/fonts/Roboto-Regular.ttf"
     let cfg = config windowIconPath robotoRegularFont
+    _ <- forkIO $ void dingBell
     void . timeout 5_000_000 $ startApp () (\_ _ _ _ -> []) (buildUI str) cfg
   where
     config icon' roboto =
@@ -32,3 +36,9 @@ buildUI str _wenv _model =
     box_ [alignCenter]
         . animFadeIn_ [autoStart, duration 4_000]
         $ label (fromString str) `styleBasic` [textSize 80.0]
+
+dingBell :: IO ExitCode
+dingBell = do
+    soundPath <- getDataFileName "assets/sound/bell.mp3"
+    (_, _, _, phandle) <- runInteractiveProcess "mpv" ["--no-video", soundPath] Nothing Nothing
+    waitForProcess phandle
