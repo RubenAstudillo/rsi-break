3254f3cc319849b02a5cc1b2f99dae71700ebaeb
Ruben Astudillo <ruben.astud@gmail.com>
2023-10-12 23:03:18 -0300

    don't use exceptions for killing a thread for store configuration

--- src/RsiBreak/Widget/Settings.hs
+++ src/RsiBreak/Widget/Settings.hs
@@ -4,24 +4,27 @@ module RsiBreak.Widget.Settings (
     buildUI,
 ) where
 
-import Control.Applicative (Alternative (empty))
 import Control.Lens (set)
 import Monomer
 import RsiBreak.Actions (storeSettingsOnConfigFile)
 import RsiBreak.Model.Minutes (Minutes)
 import RsiBreak.Model.Settings
 
-data TimerSettingEvent = TSENewWorkTime Minutes | TSENewRestTime Minutes
+data TimerChange = TSENewWorkTime Minutes | TSENewRestTime Minutes
+    deriving (Eq, Show)
+
+data TimerSettingEvent = TimerChangeEvent TimerChange | TSENoOp
     deriving (Eq, Show)
 
 handleEvent :: ep -> EventHandler TimerSetting TimerSettingEvent sp ep
-handleEvent onChangeEvent _wenv _node model evt =
+handleEvent _onChangeEvent _wenv _node _model TSENoOp = []
+handleEvent onChangeEvent _wenv _node model (TimerChangeEvent evt) =
     let newModel = case evt of
             TSENewWorkTime newm -> set workInterval newm model
             TSENewRestTime newm -> set restInterval newm model
      in [ Model newModel
         , Report onChangeEvent
-        , Task (storeSettingsOnConfigFile newModel *> empty)
+        , Task (TSENoOp <$ storeSettingsOnConfigFile newModel)
         ]
 
 buildUI :: UIBuilder TimerSetting TimerSettingEvent
@@ -29,10 +32,10 @@ buildUI _wenv _model =
     vstack
         [ hstack
             [ label "Work time: "
-            , numericField_ workInterval [minValue 0, maxValue 300, onChange TSENewWorkTime]
+            , numericField_ workInterval [minValue 0, maxValue 300, onChange (TimerChangeEvent . TSENewWorkTime)]
             ]
         , hstack
             [ label "Rest time: "
-            , numericField_ restInterval [minValue 0, maxValue 300, onChange TSENewRestTime]
+            , numericField_ restInterval [minValue 0, maxValue 300, onChange (TimerChangeEvent . TSENewRestTime)]
             ]
         ]
